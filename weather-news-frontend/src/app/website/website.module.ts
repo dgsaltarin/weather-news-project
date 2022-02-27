76e06ebe77aaa3fa543b6027162c257201ec15dd
dgsaltarin <dgsaltarin@gmail.com>
2022-02-27 15:01:42 -0500

    created component for a search bar and a component to display the weather information, also added angular grid and angular material

--- weather-news-frontend/src/app/website/website.module.ts
+++ weather-news-frontend/src/app/website/website.module.ts
@@ -2,16 +2,28 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { WebsiteRoutingModule } from "./landing-page/website-routing.module";
+import { SearchComponent } from './search/search.component';
+import { WeatherDetailsComponent } from './weather-details/weather-details.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 
 
 @NgModule({
   declarations: [
-    LandingPageComponent
+    LandingPageComponent,
+    SearchComponent,
+    WeatherDetailsComponent
   ],
   imports: [
     CommonModule,
-    WebsiteRoutingModule
+    WebsiteRoutingModule,
+    MatGridListModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule
   ]
 })
 export class WebsiteModule { }
